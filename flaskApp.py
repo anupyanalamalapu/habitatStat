0fa97feb5a69e203f51741307053ee17ac2070ce
Samir Akre <sakre@ucdavis.edu>
2018-01-21 02:18:33 -0800

    functioning summary of article

--- flaskApp.py
+++ flaskApp.py
@@ -2,17 +2,11 @@ from flask import Flask,render_template
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
+import pandas as pd
 
 app = Flask(__name__)
 # Instantiates a client
 client = language.LanguageServiceClient()
-
-# The text to analyze
-text = u'Hello, world!'
-document = types.Document(
-    content=text,
-    type=enums.Document.Type.PLAIN_TEXT)
-
 
 
 @app.route('/')
@@ -21,13 +15,37 @@ def index():
 
 @app.route('/npl')
 def npl():
-    response= client.analyze_sentiment(
-    document=document,
-    encoding_type='UTF32',
-    )
-    report=[]
-    for sent in response.sentences:
-       print sent.text.content
-       print sent.sentiment.magnitude
-       print sent.sentiment.score
-    return render_template('nplTest.html', data=report)
+    # The text to analyze
+    with open('article.txt','r') as article:
+        text=article.read()
+        document = types.Document(
+            content=text,
+            type=enums.Document.Type.PLAIN_TEXT
+        )
+
+        response= client.analyze_sentiment(
+            document=document,
+            encoding_type='UTF32',
+        )
+        report={
+            'words':    [],
+            'mags':     [],
+            'scores':   []
+        }
+        
+        
+        for sent in response.sentences:
+            words=sent.text.content
+            mag=sent.sentiment.magnitude
+            score=sent.sentiment.magnitude
+            report['words'].append(words)
+            report['mags'].append(mag)
+            report['scores'].append(score)
+        repDF=pd.DataFrame(report).sort_values('mags',ascending=False)
+        title=text[0:text.find('\n')]
+        print title
+        news=[repDF['words'][0],repDF['words'][1],repDF['words'][2]]
+        
+
+
+    return render_template('nplTest.html', data=news,title=title)
